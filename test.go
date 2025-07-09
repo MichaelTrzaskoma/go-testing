87fa23fa7b8ef7b3c9463228dd27d7a37a64a8ed
Michael Trzaskoma <mtrzaskoma@netbuilder.io>
2025-07-09 15:44:14 -0400

    Environment variable setup

--- test.go
+++ test.go
@@ -7,16 +7,19 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
 
-var templateHost string = "localhost"
-var templatePort string = "19000"
-var templateProtocol string = "http"
-var templateWG string = "default"
+// var templateHost string = "localhost"
+// var templatePort string = "19000"
+// var templateProtocol string = "http"
+// var templateWG string = "default"
 
-var baseUrl = templateProtocol + "://" + templateHost + ":" + templatePort
+// var baseUrl = templateProtocol + "://" + templateHost + ":" + templatePort
 
 type CribConfig map[string]interface{}
 
@@ -84,7 +87,7 @@ type Id string
 var id Id
 
 func (e *Id) Set(s string) error {
-	allowedCharsRegex := regexp.MustCompile("^[a-zA-Z0-9_-]+(?:\\.csv)?$")
+	allowedCharsRegex := regexp.MustCompile(`^[a-zA-Z0-9_-]+(?:\.csv)?$`)
 	if allowedCharsRegex.MatchString(s) {
 		*e = Id(s)
 		return nil
@@ -123,7 +126,7 @@ func (e WorkerGroupList) String() string {
 	return strings.Join(e, ", ")
 }
 
-func tokenApiCall(username string, password string) string {
+func tokenApiCall(baseUrl string, username string, password string) string {
 	url := baseUrl + "/api/v1/auth/login"
 	authBody := map[string]string{"username": username, "password": password}
 	authBodyJson, _ := json.Marshal(authBody)
@@ -155,7 +158,7 @@ func tokenApiCall(username string, password string) string {
 	}
 }
 
-func getWorkerGroups(token string) {
+func getWorkerGroups(baseUrl string, token string) {
 	url := baseUrl + "/api/v1/master/groups"
 
 	client := &http.Client{}
@@ -177,122 +180,6 @@ func getWorkerGroups(token string) {
 		fmt.Println(string(responseData))
 
 	}
-}
-
-func getSource(baseApiUrl string, workerGroup string, token string, id string) CribConfig {
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs/" + id
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header = http.Header{"Authorization": {token}}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil
-
-	} else {
-		// Read Body
-		responseData, err := io.ReadAll(resp.Body)
-		//Handling Read error
-		if err != nil {
-			fmt.Println(err.Error())
-			return nil
-		}
-
-		var response struct {
-			Items []CribConfig `json:"items"`
-			Count int          `json:"count"`
-		}
-
-		_ = json.Unmarshal([]byte(responseData), &response)
-
-		fmt.Println(string(responseData))
-
-		delete(response.Items[0], "status")
-		delete(response.Items[0], "notifications")
-
-		return response.Items[0]
-
-	}
-}
-
-func createSource(baseApiUrl string, workerGroup string, token string, sourceConfig []byte) {
-	client := &http.Client{}
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs"
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(sourceConfig))
-	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
-	resp, _ := client.Do(req)
-	responseData_2, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(responseData_2))
-}
-
-func updateSource(baseApiUrl string, workerGroup string, token string, id string, sourceConfig []byte) {
-	client := &http.Client{}
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs/" + id
-	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(sourceConfig))
-	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
-	resp, _ := client.Do(req)
-	responseData_2, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(responseData_2))
-}
-
-func getDest(baseApiUrl string, workerGroup string, token string, id string) CribConfig {
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs/" + id
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header = http.Header{"Authorization": {token}}
-	resp, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		return nil
-
-	} else {
-		// Read Body
-		responseData, err := io.ReadAll(resp.Body)
-		//Handling Read error
-		if err != nil {
-			fmt.Println(err.Error())
-			return nil
-		}
-
-		var response struct {
-			Items []CribConfig `json:"items"`
-			Count int          `json:"count"`
-		}
-
-		_ = json.Unmarshal([]byte(responseData), &response)
-
-		fmt.Println(string(responseData))
-
-		delete(response.Items[0], "status")
-		delete(response.Items[0], "notifications")
-
-		return response.Items[0]
-
-	}
-}
-
-func createDest(baseApiUrl string, workerGroup string, token string, destConfig []byte) {
-	client := &http.Client{}
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs"
-	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(destConfig))
-	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
-	resp, _ := client.Do(req)
-	responseData, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(responseData))
-}
-
-func updateDest(baseApiUrl string, workerGroup string, token string, id string, destConfig []byte) {
-	client := &http.Client{}
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs/" + id
-	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(destConfig))
-	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
-	resp, _ := client.Do(req)
-	responseData, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(responseData))
 }
 
 func getLookupContent(baseApiUrl string, workerGroup string, token string, lookup_id string) []byte {
@@ -478,9 +365,47 @@ func relicateConfigPatch(origBaseApiUrl string, origWorkerGroup string, origToke
 }
 
 func main() {
+	var templateProtocol, templateHost, templatePort, templateWorkerGroup, templateUser, templatePass, targetProtocol, targetHost, targetPort, targetUser, targetPass, templateUrl, targetUrl string
+
+	// Global Var Loading
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file, relying on environment variables alone")
+	}
+	templateProtocol = os.Getenv("TEMPLATE_API_PROTOCOL")
+	templateHost = os.Getenv("TEMPLATE_HOST")
+	templatePort = os.Getenv("TEMPLATE_PORT")
+	templateWorkerGroup = os.Getenv("TEMPLATE_WORKER_GROUP")
+	templateUser = os.Getenv("TEMPLATE_API_USERNAME")
+	templatePass = os.Getenv("TEMPLATE_API_PASSWORD")
+
+	flag.Var(&env, "env", "Set the env (Uat or Prod)")
+
+	if strings.ToLower(string(env)) == "prod" {
+		targetProtocol = os.Getenv("PROD_API_PROTOCOL")
+		targetHost = os.Getenv("PROD_HOST")
+		targetPort = os.Getenv("PROD_PORT")
+		targetUser = os.Getenv("PROD_API_USERNAME")
+		targetPass = os.Getenv("PROD_API_PASSWORD")
+	} else {
+		targetProtocol = os.Getenv("UAT_API_PROTOCOL")
+		targetHost = os.Getenv("UAT_HOST")
+		targetPort = os.Getenv("UAT_PORT")
+		targetUser = os.Getenv("UAT_API_USERNAME")
+		targetPass = os.Getenv("UAT_API_PASSWORD")
+	}
+
+	templateUrl = templateProtocol + "://" + templateHost
+	if len(strings.TrimSpace(templatePort)) != 0 {
+		templateUrl = templateUrl + ":" + templatePort
+	}
+
+	targetUrl = targetProtocol + "://" + targetHost
+	if len(strings.TrimSpace(targetPort)) != 0 {
+		targetUrl = targetUrl + ":" + targetPort
+	}
 
 	flag.Var(&action, "action", "Set the action (Create, Update, or Delete)")
-	flag.Var(&env, "env", "Set the env (Uat or Prod)")
 	flag.Var(&objType, "objType", "Defines the object type of the configuration item that we are targeting (Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup)")
 	flag.Var(&id, "id", "Set the id for configuration item you're looking to target")
 	flag.Var(&wgList, "wgList", "List of worker groups to target")
@@ -492,11 +417,12 @@ func main() {
 	fmt.Println("Selected id:", id)
 	fmt.Println("Selected worker group(s) are:", wgList)
 
-	token := tokenApiCall("admin", "Test1234")
+	templateToken := tokenApiCall(templateUrl, templateUser, templatePass)
+	targetToken := tokenApiCall(targetUrl, targetUser, targetPass)
 	//fmt.Println("Token here:", val)
 
 	// getWorkerGroups(token)
-	relicateConfigPatch(baseUrl, "default", token, baseUrl, wgList, token, string(objType), string(id))
+	relicateConfigPatch(templateUrl, templateWorkerGroup, templateToken, targetUrl, wgList, targetToken, string(objType), string(id))
 	// getLookup(baseUrl, "default", token, "test.csv")
 	// getSourceConfig := getSource(token)
 	// fmt.Print(getSourceConfig)
