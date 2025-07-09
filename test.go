5a9b1d05224e8bc002fdbe677b8d8e03d5b75f36
Michael Trzaskoma <mtrzaskoma@netbuilder.io>
2025-07-09 12:37:23 -0400

    Lookup feature added

--- test.go
+++ test.go
@@ -65,11 +65,11 @@ var objType ObjType
 
 func (e *ObjType) Set(s string) error {
 	switch strings.ToLower(s) {
-	case "Source", "Destination", "Pipeline", "Pack", "GlobalVariable", "Lookup":
+	case "source", "destination", "pipeline", "pack", "globalvariable", "lookup":
 		*e = ObjType(s)
 		return nil
 	default:
-		return fmt.Errorf("invalid env: %s. Valid options are: Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup", s)
+		return fmt.Errorf("invalid Object Type provided: %s. Valid options are: Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup", s)
 	}
 
 }
@@ -84,12 +84,12 @@ type Id string
 var id Id
 
 func (e *Id) Set(s string) error {
-	allowedCharsRegex := regexp.MustCompile("^[a-zA-Z0-9_-]+$")
+	allowedCharsRegex := regexp.MustCompile("^[a-zA-Z0-9_-]+(?:\\.csv)?$")
 	if allowedCharsRegex.MatchString(s) {
 		*e = Id(s)
 		return nil
 	} else {
-		return fmt.Errorf("invalid Id String: %s. String must be alphanumeric with special characters '-' and '_' allowed", s)
+		return fmt.Errorf("invalid Id String: %s. String must be alphanumeric with special characters '-' and '_' allowed or if a lookup, ensure ending is .csv", s)
 	}
 
 }
@@ -123,9 +123,9 @@ func (e WorkerGroupList) String() string {
 	return strings.Join(e, ", ")
 }
 
-func tokenApiCall() string {
+func tokenApiCall(username string, password string) string {
 	url := baseUrl + "/api/v1/auth/login"
-	authBody := map[string]string{"username": "admin", "password": "Test1234"}
+	authBody := map[string]string{"username": username, "password": password}
 	authBodyJson, _ := json.Marshal(authBody)
 	client := &http.Client{}
 	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(authBodyJson))
@@ -179,8 +179,8 @@ func getWorkerGroups(token string) {
 	}
 }
 
-func getSource(token string) CribConfig {
-	url := baseUrl + "/api/v1/m/" + templateWG + "/system/inputs/splunk_uf"
+func getSource(baseApiUrl string, workerGroup string, token string, id string) CribConfig {
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs/" + id
 
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
@@ -199,9 +199,7 @@ func getSource(token string) CribConfig {
 			fmt.Println(err.Error())
 			return nil
 		}
-		//var result map[string]interface{}
 
-		// _ = json.Unmarshal([]byte(responseData), &result)
 		var response struct {
 			Items []CribConfig `json:"items"`
 			Count int          `json:"count"`
@@ -213,27 +211,8 @@ func getSource(token string) CribConfig {
 
 		delete(response.Items[0], "status")
 		delete(response.Items[0], "notifications")
-		// fmt.Println(response.Items[0])
 
 		return response.Items[0]
-
-		// response.Items[0]["id"] = "test"
-		// response.Items[0]["port"] = 1923
-
-		// jsonBytes, _ := json.Marshal(response.Items[0])
-		// jsonString := string(jsonBytes)
-
-		// fmt.Println(jsonString)
-		// // fmt.Println((result["items"]))
-
-		// url_2 := base_url + "/api/v1/m/" + template_wg + "/system/inputs"
-
-		// sourceBodyJson, _ := json.Marshal(response.Items[0])
-		// req, _ := http.NewRequest("POST", url_2, bytes.NewBuffer(sourceBodyJson))
-		// req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
-		// resp, _ := client.Do(req)
-		// responseData_2, _ := io.ReadAll(resp.Body)
-		// fmt.Println(string(responseData_2))
 
 	}
 }
@@ -248,14 +227,254 @@ func createSource(baseApiUrl string, workerGroup string, token string, sourceCon
 	fmt.Println(string(responseData_2))
 }
 
-func updateSource(baseApiUrl string, workerGroup string, token string, sourceConfig []byte) {
+func updateSource(baseApiUrl string, workerGroup string, token string, id string, sourceConfig []byte) {
 	client := &http.Client{}
-	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs/test"
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/inputs/" + id
 	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(sourceConfig))
 	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
 	resp, _ := client.Do(req)
 	responseData_2, _ := io.ReadAll(resp.Body)
 	fmt.Println(string(responseData_2))
+}
+
+func getDest(baseApiUrl string, workerGroup string, token string, id string) CribConfig {
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs/" + id
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header = http.Header{"Authorization": {token}}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+
+	} else {
+		// Read Body
+		responseData, err := io.ReadAll(resp.Body)
+		//Handling Read error
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil
+		}
+
+		var response struct {
+			Items []CribConfig `json:"items"`
+			Count int          `json:"count"`
+		}
+
+		_ = json.Unmarshal([]byte(responseData), &response)
+
+		fmt.Println(string(responseData))
+
+		delete(response.Items[0], "status")
+		delete(response.Items[0], "notifications")
+
+		return response.Items[0]
+
+	}
+}
+
+func createDest(baseApiUrl string, workerGroup string, token string, destConfig []byte) {
+	client := &http.Client{}
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs"
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(destConfig))
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
+	resp, _ := client.Do(req)
+	responseData, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(responseData))
+}
+
+func updateDest(baseApiUrl string, workerGroup string, token string, id string, destConfig []byte) {
+	client := &http.Client{}
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/outputs/" + id
+	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(destConfig))
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
+	resp, _ := client.Do(req)
+	responseData, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(responseData))
+}
+
+func getLookupContent(baseApiUrl string, workerGroup string, token string, lookup_id string) []byte {
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/lookups/" + lookup_id + "/content?raw=0"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	} else {
+		// Read Body
+		responseData, err := io.ReadAll(resp.Body)
+		//Handling Read error
+		if err != nil {
+			fmt.Println(err.Error())
+
+		}
+
+		// var response struct {
+		// 	Items []CribConfig `json:"items"`
+		// 	Count int          `json:"count"`
+		// }
+
+		fmt.Println(string(responseData))
+		return responseData
+	}
+
+}
+
+func uploadLookup(baseApiUrl string, workerGroup string, token string, lookup_id string, lookupContent []byte) []byte {
+	client := &http.Client{}
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + "/system/lookups/?filename=" + lookup_id
+	//objectConfigBytes, _ := json.Marshal(responseData)
+	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(lookupContent))
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"text/csv"}}
+	resp, _ := client.Do(req)
+	responseData, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(string(responseData))
+
+	var lookupPayloadFileinfo struct {
+		FileName string `json:"filename"`
+	}
+
+	type fileInfo struct {
+		Filename string `json:"filename"`
+	}
+	type LookupPayload struct {
+		Id       string   `json:"id"`
+		FileInfo fileInfo `json:"fileInfo"`
+	}
+	_ = json.Unmarshal([]byte(responseData), &lookupPayloadFileinfo)
+
+	lookupApiPayload := LookupPayload{
+		Id: lookup_id,
+		FileInfo: fileInfo{
+			Filename: lookupPayloadFileinfo.FileName,
+		},
+	}
+
+	lookUpPayloadJson, _ := json.Marshal(lookupApiPayload)
+	return lookUpPayloadJson
+}
+
+func patchLookup(baseApiUrl string, workerGroup string, token string, lookup_id string, patchPayload []byte) {
+	client := &http.Client{}
+	url := baseApiUrl + "/api/v1/m/" + "GroupA" + "/system/lookups/" + lookup_id
+	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(patchPayload))
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
+	resp, _ := client.Do(req)
+	responseData, _ := io.ReadAll(resp.Body)
+	print(responseData)
+
+}
+
+func getDataObj(baseApiUrl string, workerGroup string, token string, id string, objType string) CribConfig {
+
+	var objEndpoint string
+
+	switch strings.ToLower(objType) {
+	case "source":
+		objEndpoint = "/system/inputs"
+	case "destination":
+		objEndpoint = "/system/outputs"
+	case "pipeline":
+		objEndpoint = "/pipelines"
+	case "globalvariable":
+		objEndpoint = "/lib/vars"
+	default:
+		fmt.Printf("invalid Object Type provided: %s. Valid options are: Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup", objType)
+	}
+
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + objEndpoint + "/" + id
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header = http.Header{"Authorization": {token}}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+
+	} else {
+		// Read Body
+		responseData, err := io.ReadAll(resp.Body)
+		//Handling Read error
+		if err != nil {
+			fmt.Println(err.Error())
+			return nil
+		}
+
+		var response struct {
+			Items []CribConfig `json:"items"`
+			Count int          `json:"count"`
+		}
+
+		_ = json.Unmarshal([]byte(responseData), &response)
+
+		fmt.Println(string(responseData))
+
+		delete(response.Items[0], "status")
+		delete(response.Items[0], "notifications")
+
+		return response.Items[0]
+
+	}
+}
+
+func updateDataObj(baseApiUrl string, workerGroup string, token string, id string, objConfig []byte, objType string) {
+	var objEndpoint string
+
+	switch strings.ToLower(objType) {
+	case "source":
+		objEndpoint = "/system/inputs"
+	case "destination":
+		objEndpoint = "/system/outputs"
+	case "pipeline":
+		objEndpoint = "/pipelines"
+	case "globalvariable":
+		objEndpoint = "/lib/vars"
+	default:
+		fmt.Printf("invalid Object Type provided: %s. Valid options are: Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup", objType)
+	}
+
+	client := &http.Client{}
+	url := baseApiUrl + "/api/v1/m/" + workerGroup + objEndpoint + "/" + id
+	req, _ := http.NewRequest("PATCH", url, bytes.NewBuffer(objConfig))
+	req.Header = http.Header{"Authorization": {token}, "content-type": {"application/json"}}
+	resp, _ := client.Do(req)
+	responseData, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(responseData))
+}
+
+func relicateConfigPatch(origBaseApiUrl string, origWorkerGroup string, origToken string, targetBaseApiUrl string, targetWorkerGroups []string, targetToken string, objType string, objId string) {
+
+	switch strings.ToLower(objType) {
+	case "source", "destination", "pipeline", "globalvariable":
+		objectConfig := getDataObj(origBaseApiUrl, origWorkerGroup, origToken, objId, objType)
+		objectConfig["port"] = 1953 // testing
+		objectConfigBytes, _ := json.Marshal(objectConfig)
+		for _, workerGroup := range targetWorkerGroups {
+			updateDataObj(targetBaseApiUrl, workerGroup, targetToken, objId, objectConfigBytes, objType)
+		}
+	case "lookup":
+		if strings.HasSuffix(objId, ".csv") {
+			objectContent := getLookupContent(origBaseApiUrl, origWorkerGroup, origToken, objId)
+			for _, workerGroup := range targetWorkerGroups {
+				objectUpload := uploadLookup(targetBaseApiUrl, workerGroup, targetToken, objId, objectContent)
+				patchLookup(targetBaseApiUrl, workerGroup, targetToken, objId, objectUpload)
+
+			}
+		} else {
+			fmt.Println("Error: Expected object Id for lookup to end with '.csv', invalid lookup submitted")
+		}
+
+	default:
+		fmt.Println("Not valid, ignored")
+	}
 }
 
 func main() {
@@ -265,24 +484,27 @@ func main() {
 	flag.Var(&objType, "objType", "Defines the object type of the configuration item that we are targeting (Source, Destination, Pipeline, Pack, GlobalVariable, or Lookup)")
 	flag.Var(&id, "id", "Set the id for configuration item you're looking to target")
 	flag.Var(&wgList, "wgList", "List of worker groups to target")
+
 	flag.Parse()
 	fmt.Println("Selected action:", action)
 	fmt.Println("Selected env:", env)
-	fmt.Println("Selected object type", objType)
+	fmt.Println("Selected object type:", objType)
 	fmt.Println("Selected id:", id)
 	fmt.Println("Selected worker group(s) are:", wgList)
 
-	token := tokenApiCall()
+	token := tokenApiCall("admin", "Test1234")
 	//fmt.Println("Token here:", val)
 
-	getWorkerGroups(token)
-	getSourceConfig := getSource(token)
-	fmt.Print(getSourceConfig)
+	// getWorkerGroups(token)
+	relicateConfigPatch(baseUrl, "default", token, baseUrl, wgList, token, string(objType), string(id))
+	// getLookup(baseUrl, "default", token, "test.csv")
+	// getSourceConfig := getSource(token)
+	// fmt.Print(getSourceConfig)
 
-	getSourceConfig["id"] = "test"
-	getSourceConfig["port"] = 1943
-	sourceConfigBytes, _ := json.Marshal(getSourceConfig)
-	//createSource(baseUrl, templateWG, token, sourceConfigBytes)
-	updateSource(baseUrl, templateWG, token, sourceConfigBytes)
+	// getSourceConfig["id"] = "test"
+	// getSourceConfig["port"] = 1943
+	// sourceConfigBytes, _ := json.Marshal(getSourceConfig)
+	// //createSource(baseUrl, templateWG, token, sourceConfigBytes)
+	// updateSource(baseUrl, templateWG, token, sourceConfigBytes)
 
 }
